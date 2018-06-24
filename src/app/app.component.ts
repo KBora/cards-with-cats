9b71e46536656ba2f7ceba36fc38f28e7c42e67b
wookkii <wookkii@home>
2018-06-24 14:24:23 +1000

    Implement card display and active status toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Card } from "src/app/card/card";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Cards with Cats ';
+
+  cards: Card[] = [
+    new Card(1, false, null),
+    new Card(2, false, null),
+    new Card(3, false, null),
+    new Card(4, false, null)
+  ];
+  activeCard: Card = null;
+  fetchingCats: boolean = false;
+
+  private activeCardPresent() {
+    return this.activeCard !== null;
+  }
+
+  onClickCard(card) {
+
+    // Toggle active card if one exists and is not the card that was clicked on
+    if (this.activeCardPresent() && this.activeCard !== card) {
+      this.activeCard.toggleStatus();
+    }
+
+    // Reset active card
+    this.activeCard = null;
+
+    // Toggle card
+    card.toggleStatus();
+
+    // Mark state
+    if (card.isActive) {
+      this.activeCard = card;
+    }
+
+  }
+
 }
